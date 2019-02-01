de474c8f4b1b9bfde3e0d43e12da806c66c45b52
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-01 18:01:05 +0100

    Initialize Google sign in status on load

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load('client:auth2', async () => {
+      await window.gapi.client.init({
+        clientId:
+          '144298741452-j6i1pjdfg0h7qd6nak2gdkl7k8o597jl.apps.googleusercontent.com',
+        scope: 'email'
+      });
+
+      this.auth = window.gapi.auth2.getAuthInstance();
+      this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>Unsure if signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>signed in!</div>;
+    } else {
+      return <div>Not signed in!</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return this.renderAuthButton();
   }
 }
 
