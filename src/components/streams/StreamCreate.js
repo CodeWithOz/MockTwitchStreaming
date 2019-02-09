5fc221041743aad041de9dfbd8f9f83c4958e907
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-09 22:45:55 +0100

    Validate user input when interacting with form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
-  renderInput({ input, label }) {
+  renderInput({ input, label, meta }) {
     return (
       <div className="field">
         <label>{label}</label>
         <input {...input} />
+        <div>{meta.error}</div>
       </div>
     );
   }
@@ -33,8 +34,23 @@ class StreamCreate extends Component {
   }
 }
 
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+
+  return errors;
+};
+
 const WrappedFormComponent = reduxForm({
-  form: 'StreamCreate'
+  form: 'StreamCreate',
+  validate
 })(StreamCreate);
 
 export default props => <WrappedFormComponent {...props} />;
